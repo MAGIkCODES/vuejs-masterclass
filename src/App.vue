27b76fca526dbf2cb7f422cac94bca7aaec7ea33
MAGIKCODES <justiceodoyi@gmail.com>
2024-11-02 17:25:37 +0100

    unplugin vue router routing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>Welcome to pulse!</h1>
+  <router-view />
 </template>
 
 <style scoped></style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
+// import HomeView from '@/views/HomeView.vue'
+// import { h } from 'vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes,
+  //     {
+  //       path: '/',
+  //       name: 'home',
+  //       component: HomeView,
+  //     },
+  //     {
+  //       path: '/projects',
+  //       name: 'projects',
+  //       component: import('@/views/ProjectsView.vue'),
+  //     },
+  //     {
+  //       //dynamic route
+  //       path: '/projects/:id',
+  //       name: 'single-project',
+  //       component: import('@/views/SingleProjectView.vue'),
+  //     },
+  //     {
+  //       // catch all 404 page
+  //       path: '/:catchAll(.*)',
+  //       name: 'not-found',
+  //       component: h('p', { style: 'color: red' }, 'Not Found'),
+  //     },
+  // {
+  //   //catch a 404 for a specific route
+  //   path: '/projects/:catchAll(.*)',
+  //   name: 'not-found',
+  //   component: h('p', { style: 'color: red' }, '404 Project Not Found'),
+  // },
 })
 
 export default router
